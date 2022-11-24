add45b797acd02ebc6d762070ce1ac5d046a77f4
DoWhatYouL0ve <william_ross@mailfence.com>
2022-11-24 12:44:42 +0100

    fix: 404 page - automatic redirection to home page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,12 +1,24 @@
 import Link from "next/link";
+import {useEffect} from "react";
+import {useRouter} from "next/router";
 
 const NotfoundPage = () => {
+
+    const router = useRouter()
+
+    useEffect(()=>{
+        const timeout = setTimeout(() => {
+            router.push('/')
+        }, 5000)
+        return ()=>clearTimeout(timeout)
+    }, [])
+
   return (
       <>
           <div className={'not-found'}>
             <h1>Ops...</h1>
             <h2>Sorry, this page doesn&apos;t exist!</h2>
-              <p>Go to <Link href={'/'}><span>home page</span></Link></p>
+              <p>Please click <Link href={'/'}><span>go tohome page</span></Link> if it won&apos;t redirect you automatically in 5 seconds</p>
           </div>
       </>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1 className={`${styles.title} font-effect-fire-animation`}>Main Page</h1>
+      <h1 className={`${styles.title} font-effect-fire-animation`}>Best Burgers here!</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet, dolorem doloremque doloribus error excepturi maiores maxime necessitatibus nihil nobis placeat quas suscipit. Accusantium aut distinctio dolorem harum maiores velit?</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur ducimus explicabo inventore laboriosam omnis praesentium reiciendis voluptate voluptates voluptatum. Delectus, pariatur rerum! Assumenda, ducimus libero odio rerum sint tenetur totam?</p>
       <Link href={'/burgers'}><span className={styles.btn}>Burgers menu</span></Link>
