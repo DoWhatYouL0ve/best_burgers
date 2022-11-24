5c1607fd966546cbadcd61bdf562b44cdb34f286
DoWhatYouL0ve <william_ross@mailfence.com>
2022-11-24 10:38:30 +0100

    fix: layout - added css styles to the project

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,11 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Main Page</h1>
-        <Link href={'/burgers'}>Burgers menu</Link>
+    <div className={styles.container}>
+      <h1 className={`${styles.title} font-effect-fire-animation`}>Main Page</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet, dolorem doloremque doloribus error excepturi maiores maxime necessitatibus nihil nobis placeat quas suscipit. Accusantium aut distinctio dolorem harum maiores velit?</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur ducimus explicabo inventore laboriosam omnis praesentium reiciendis voluptate voluptates voluptatum. Delectus, pariatur rerum! Assumenda, ducimus libero odio rerum sint tenetur totam?</p>
+      <Link href={'/burgers'}><span className={styles.btn}>Burgers menu</span></Link>
     </div>
   )
 }
